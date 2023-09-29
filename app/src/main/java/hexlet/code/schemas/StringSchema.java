08e3b23cdbf726a0a13cf64701e9a75aad7af1ef
Evgeny_Ch <gek_sh@mail.ru>
2023-09-29 23:54:25 +0300

    Updated StringSchema with Baseschema

--- app/src/main/java/hexlet/code/schemas/StringSchema.java
+++ app/src/main/java/hexlet/code/schemas/StringSchema.java
@@ -2,17 +2,19 @@ package hexlet.code.schemas;
 
 import java.util.ArrayList;
 
-public class StringSchema {
-    private boolean required = false;
+public class StringSchema extends BaseSchema {
+//    private boolean required = false;
+    private boolean minLengthEnabled = false;
     private int minLength = 0;
     private ArrayList<String> containsStr = new ArrayList<>();
 
-    public StringSchema required() {
-        this.required = true;
-        return this;
-    }
+//    public StringSchema required() {
+//        this.required = true;
+//        return this;
+//    }
 
     public StringSchema minLength(int length) {
+        this.minLengthEnabled = true;
         this.minLength = length;
         return this;
     }
@@ -25,9 +27,9 @@ public class StringSchema {
         return this;
     }
 
-    public boolean isRequired() {
-        return required;
-    }
+//    public boolean isRequiredNotNull() {
+//        return required;
+//    }
     public int getMinLength() {
         return minLength;
     }
@@ -36,29 +38,36 @@ public class StringSchema {
         return containsStr;
     }
 
-    public boolean isValid(Object str) {
-        boolean rezult = true;
-        if ((str == null) && isRequired()) {
-            rezult = false;
+    public boolean isMinLengthEnabled() {
+        return minLengthEnabled;
+    }
+
+    public boolean isValid(Object validationObject) {
+        boolean result = true;
+        if (!super.isValid(validationObject)) {
+            result = false;
         }
-        if (rezult && !(str == null) && (!(str instanceof String))) {
-            rezult = false;
+//        if ((validationObject == null) && isRequiredNotNull()) {
+//            result = false;
+//        }
+        if (result && !(validationObject == null) && (!(validationObject instanceof String))) {
+            result = false;
         }
-        if (rezult && !(str == null) && (str instanceof String) && (str.toString().isEmpty())
-                && isRequired()) {
-            rezult = false;
+        if (result && (validationObject instanceof String)
+                && (validationObject.toString().isEmpty()) && isRequiredNotNull()) {
+            result = false;
         }
-        if (rezult && !(str == null) && (str instanceof String)
-                && (str.toString().length() < getMinLength())) {
-            rezult = false;
+        if (result && (validationObject instanceof String) && isMinLengthEnabled()
+                && (validationObject.toString().length() < getMinLength())) {
+            result = false;
         }
-        if (rezult && !(str == null) && (str instanceof String) && (!getContainsStr().isEmpty())) {
+        if (result && (validationObject instanceof String) && (!getContainsStr().isEmpty())) {
             for (String oneStr: getContainsStr()) {
-                if (!((String) str).contains(oneStr)) {
-                    rezult = false;
+                if (!((String) validationObject).contains(oneStr)) {
+                    result = false;
                 }
             }
         }
-        return rezult;
+        return result;
     }
 }
