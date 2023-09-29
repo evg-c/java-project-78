48379cdced2e3d4cbe3e95645d35e556e28c82e2
Evgeny_Ch <gek_sh@mail.ru>
2023-09-30 01:34:13 +0300

    Added map

--- app/src/main/java/hexlet/code/Validator.java
+++ app/src/main/java/hexlet/code/Validator.java
@@ -1,5 +1,5 @@
 package hexlet.code;
-
+import hexlet.code.schemas.MapSchema;
 import hexlet.code.schemas.NumberSchema;
 import hexlet.code.schemas.StringSchema;
 
@@ -10,5 +10,9 @@ public class Validator {
 
     public NumberSchema number() {
         return new NumberSchema();
+    }
+
+    public MapSchema map() {
+        return new MapSchema();
     }
 }
