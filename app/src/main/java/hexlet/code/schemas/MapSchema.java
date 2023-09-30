3e0b6a46c5bb21010092902e0c30fe2ab1c43d3c
Evgeny_Ch <gek_sh@mail.ru>
2023-10-01 01:35:12 +0300

    Added shape metod and validation of schemas

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -1,10 +1,12 @@
 package hexlet.code.schemas;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class MapSchema extends BaseSchema {
     private boolean sizeOfMapEnabled = false;
     private int sizeOfMap = 0;
+    private Map<String, BaseSchema> mapSchemas = new HashMap<>();
 
     public boolean isSizeOfMapEnabled() {
         return sizeOfMapEnabled;
@@ -20,6 +22,17 @@ public class MapSchema extends BaseSchema {
         return this;
     }
 
+    public Map<String, BaseSchema> getMapSchemas() {
+        return mapSchemas;
+    }
+
+    public Map<String, BaseSchema> shape(Map<String, BaseSchema> listSchemas) {
+        if (!(listSchemas == null)) {
+            mapSchemas.putAll(listSchemas);
+        }
+        return mapSchemas;
+    }
+
     public boolean isValid(Object objectValidation) {
         boolean result = super.isValid(objectValidation);
         if (result && !(objectValidation == null) && !(objectValidation instanceof Map<?, ?>)) {
@@ -28,6 +41,18 @@ public class MapSchema extends BaseSchema {
         if (result && (objectValidation instanceof Map<?, ?>) && isSizeOfMapEnabled()
             && (((Map<?, ?>) objectValidation).size() < getSizeOfMap())) {
             result = false;
+        }
+        if (result && (objectValidation instanceof Map<?, ?>) && (!getMapSchemas().isEmpty())) {
+            for (Map.Entry<String, BaseSchema> entryCurrentSchema: getMapSchemas().entrySet()) {
+                String currentKeySchema = entryCurrentSchema.getKey();
+                BaseSchema currentSchema = entryCurrentSchema.getValue();
+                for (Map.Entry<String, Object> entryCurrentObjectValidation
+                        : ((Map<String, Object>) objectValidation).entrySet()) {
+                    if (entryCurrentObjectValidation.getKey().equals(currentKeySchema)) {
+                        result = currentSchema.isValid(entryCurrentObjectValidation.getValue());
+                    }
+                }
+            }
         }
         return result;
     }
